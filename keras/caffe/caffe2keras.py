787f79d1e51a4745ca84d83c4e0cf582435a26e9
Hasnain Vohra <hasnain.vohra@visionsystemsinc.com>
2016-02-17 15:59:06 -0500

    added support for sequential model

--- keras/caffe/caffe2keras.py
+++ keras/caffe/caffe2keras.py
@@ -2,7 +2,7 @@ import keras.caffe.convert as convert
 import pprint
 import argparse
 
-""" 
+"""
 
     USAGE EXAMPLE
         python caffe2keras.py -load_path 'models/' -prototxt 'train_val_for_keras.prototxt' -caffemodel 'bvlc_googlenet.caffemodel'
@@ -18,6 +18,8 @@ parser.add_argument('-caffemodel', type=str,
                    help='name of the .caffemodel file')
 parser.add_argument('-store_path', type=str, default='',
                    help='path to the folder where the Keras model will be stored (default: -load_path).')
+parser.add_argument('-network_type', type=str, default='Sequential',
+                   help='Type of network. Sequential / Graph (default is Sequential)')
 parser.add_argument('-debug', action='store_true', default=0,
 		   help='use debug mode')
 
@@ -31,9 +33,11 @@ def main(args):
         store_path = args.store_path
 
     print("Converting model...")
-    model = convert.caffe_to_keras(args.load_path+'/'+args.prototxt, args.load_path+'/'+args.caffemodel, debug=args.debug)
+    model = convert.caffe_to_keras(args.load_path+'/'+args.prototxt, args.load_path+'/'+args.caffemodel,
+                                   args.network_type, debug=args.debug)
+
     print("Finished converting model.")
-    
+
     # Save converted model structure
     print("Storing model...")
     json_string = model.to_json()
--- keras/caffe/convert.py
+++ keras/caffe/convert.py
@@ -4,7 +4,7 @@ from ..layers.convolutional import *
 from ..layers.core import *
 from ..layers.normalization import *
 
-from ..models import Graph
+from ..models import Graph, Sequential
 
 from . import caffe_pb2 as caffe
 import google.protobuf
@@ -13,14 +13,13 @@ from .extra_layers import *
 
 import numpy as np
 
-
-def caffe_to_keras(prototext, caffemodel, phase='train', debug=False):
+def caffe_to_keras(prototext, caffemodel, network_type, phase='train', debug=False):
         '''
             Converts a Caffe Graph into a Keras Graph
             prototext: model description file in caffe
             caffemodel: stored weights file
             phase: train or test
-            
+
             Usage:
                 model = caffe_to_keras('VGG16.prototxt', 'VGG16_700iter.caffemodel')
         '''
@@ -37,14 +36,17 @@ def caffe_to_keras(prototext, caffemodel, phase='train', debug=False):
 
         if(debug):
             print("CREATING MODEL")
-        model = create_model(layers,
-                                  0 if phase == 'train' else 1,
-                                  tuple(config.input_dim[1:]),
-				  debug)
-        
+
+        if network_type == 'Sequential':
+            model = create_model_sequential(layers, 0 if phase == 'train' else 1, tuple(config.input_dim[1:]), debug)
+        elif network_type == 'Graph':
+            model = create_model_graph(layers, 0 if phase == 'train' else 1, tuple(config.input_dim[1:]), debug)
+        else:
+            raise NameError('Unknown network type: %s' % network_type)
+
         params = caffe.NetParameter()
         params.MergeFromString(open(caffemodel, 'rb').read())
-        
+
         if len(params.layers) != 0:
             param_layers = params.layers[:]    # V1
             v = 'V1'
@@ -59,18 +61,142 @@ def caffe_to_keras(prototext, caffemodel, phase='train', debug=False):
             print("LOADING WEIGHTS")
         weights = convert_weights(param_layers, v, debug)
 
-        load_weights(model, weights)
+        load_weights(model, weights, network_type)
 
         return model
 
-
-def create_model(layers, phase, input_dim, debug=False):
+def create_model_sequential(layers, phase, input_dim, debug=False):
     '''
-        layers: 
+    layers:
+        a list of all the layers in the mode
+    phase:
+        parameter to specify which network to extract: training or test
+    input_dim:
+        input  dimensions of the configuration (if model is in deploy mode)
+    '''
+
+    if input_dim == ():
+        in_deploy_mode = False
+    else:
+        in_deploy_mode = True
+
+    network = parse_network(layers, phase)
+
+    if len(network) == 0:
+        raise Exception('failed to construct network from the prototext')
+
+    # inputs of the network - 'in-order' is zero
+    inputs = get_inputs(network)
+    # outputs of the network - 'out-order' is zero
+    network_outputs = get_outputs(network)
+
+    # path from input to loss layers (label) removed
+    network = remove_label_paths(layers, network, inputs, network_outputs)
+
+    # while network contains what nodes follow a perticular node.
+    # we need to know what feeds a given node, hence reverse it.
+    inputs_to = reverse(network)
+
+    model = Sequential()
+
+    for layer_nb in network:
+        layer = layers[layer_nb]
+        name = layer.name
+        type_of_layer = layer_type(layer)
+
+        if type_of_layer == 'concat':
+            axis = layer.concat_param.axis
+            model.add(Activation('linear', name=name))
+
+        elif type_of_layer == 'convolution':
+            nb_filter = layer.convolution_param.num_output
+            nb_col = (layer.convolution_param.kernel_size or [layer.convolution_param.kernel_h])[0]
+            nb_row = (layer.convolution_param.kernel_size or [layer.convolution_param.kernel_w])[0]
+            stride_h = (layer.convolution_param.stride or [layer.convolution_param.stride_h])[0] or 1
+            stride_w = (layer.convolution_param.stride or [layer.convolution_param.stride_w])[0] or 1
+            pad_h = (layer.convolution_param.pad or [layer.convolution_param.pad_h])[0]
+            pad_w = (layer.convolution_param.pad or [layer.convolution_param.pad_w])[0]
+
+            if layer_nb == 0:
+                if pad_h + pad_w > 0:
+                    model.add(ZeroPadding2D(padding=(pad_h, pad_w), input_shape=input_dim))
+                    model.add(Convolution2D(nb_filter, nb_row, nb_col, subsample=(stride_h, stride_w), name=name))
+                else:
+                    model.add(Convolution2D(nb_filter, nb_row, nb_col, subsample=(stride_h, stride_w), input_shape=input_dim, name=name))
+            else:
+                if pad_h + pad_w > 0:
+                    model.add(ZeroPadding2D(padding=(pad_h, pad_w)))
+                model.add(Convolution2D(nb_filter, nb_row, nb_col, subsample=(stride_h, stride_w), name=name))
+
+        elif type_of_layer == 'dropout':
+            prob = layer.dropout_param.dropout_ratio
+            model.add(Dropout(prob, name=name))
+
+        elif type_of_layer == 'flatten':
+            model.add(Flatten(name=name))
+
+        elif type_of_layer == 'innerproduct':
+            output_dim = layer.inner_product_param.num_output
+            if len(model.layers[-1].output_shape[1:]) > 1:
+                model.add(Flatten())
+            model.add(Dense(output_dim, name=name))
+
+        elif type_of_layer == 'lrn':
+            alpha = layer.lrn_param.alpha
+            k = layer.lrn_param.k
+            beta = layer.lrn_param.beta
+            n = layer.lrn_param.local_size
+
+            model.add(LRN2D(alpha=alpha, k=k, beta=beta, n=n, name=name))
+
+        elif type_of_layer == 'pooling':
+            kernel_h = layer.pooling_param.kernel_size or layer.pooling_param.kernel_h
+            kernel_w = layer.pooling_param.kernel_size or layer.pooling_param.kernel_w
+
+            # caffe defaults to 1, hence both of the params can be zero. 'or 1'
+            stride_h = layer.pooling_param.stride or layer.pooling_param.stride_h or 1
+            stride_w = layer.pooling_param.stride or layer.pooling_param.stride_w or 1
+
+            pad_h = layer.pooling_param.pad or layer.pooling_param.pad_h
+            pad_w = layer.pooling_param.pad or layer.pooling_param.pad_w
+
+            if pad_h + pad_w > 0:
+                model.add(ZeroPadding2D(padding=(pad_h, pad_w), name=name))
+                input_layer_name = name + '_zeropadding'
+            if(layer.pooling_param.pool == 0): # MAX pooling
+                model.add(MaxPooling2D(pool_size=(kernel_h, kernel_w), name=name))
+
+            elif(layer.pooling_param.pool == 1): # AVE pooling
+                model.add(AveragePooling2D(pool_size=(kernel_h, kernel_w), strides=(stride_h, stride_w), name=name))
+            else: # STOCHASTIC?
+                raise NotImplementedError("Only MAX and AVE pooling are implemented in keras!")
+
+        elif type_of_layer == 'relu':
+            model.add(Activation('relu', name=name))
+
+        elif type_of_layer == 'sigmoid':
+            model.add(Activation('sigmoid', name=name))
+
+        elif type_of_layer == 'softmax' or type_of_layer == 'softmaxwithloss':
+            model.add(Activation('softmax', name=name))
+
+        elif type_of_layer == 'split':
+            model.add(Activation('linear', name=name))
+
+        elif type_of_layer == 'tanh':
+            model.add(Activation('tanh', name=name))
+        else:
+            raise RuntimeError('layer type', type_of_layer, 'used in this model is not currently supported')
+
+    return model
+
+def create_model_graph(layers, phase, input_dim, debug=False):
+    '''
+        layers:
             a list of all the layers in the model
-        phase: 
+        phase:
             parameter to specify which network to extract: training or test
-        input_dim: 
+        input_dim:
             `input dimensions of the configuration (if in model is in deploy mode)
     '''
     # DEPLOY MODE: first layer is computation (conv, dense, etc..)
@@ -82,8 +208,8 @@ def create_model(layers, phase, input_dim, debug=False):
 
     # obtain the nodes that make up the graph
     # returned in linked list (not matrix) representation (dictionary here)
-    network = parse_network(layers, phase)  
-    
+    network = parse_network(layers, phase)
+
     if len(network) == 0:
         raise Exception('failed to construct network from the prototext')
 
@@ -91,7 +217,7 @@ def create_model(layers, phase, input_dim, debug=False):
     inputs = get_inputs(network)
     # outputs of the network - 'out-order' is zero
     network_outputs = get_outputs(network)
-    
+
     # path from input to loss layers (label) removed
     network = remove_label_paths(layers, network, inputs, network_outputs)
 
@@ -108,9 +234,9 @@ def create_model(layers, phase, input_dim, debug=False):
             dim = input_dim
         else:
             dim = get_data_dim(layers[network_input])
-        name = 'input_'+name
+        #name = 'input_'+name
         model.add_input(name=name, input_shape=dim)
-       
+
     # parse all the layers and build equivalent keras graph
     for layer_nb in network:
         layer = layers[layer_nb]
@@ -120,13 +246,13 @@ def create_model(layers, phase, input_dim, debug=False):
         if is_data_input(layer):
             continue
 
-        # DEPLOY MODE: this layer takes in input as per the model. 
+        # DEPLOY MODE: this layer takes in input as per the model.
         # No data layer preceding it.
         # we use the actual name of the layer in 'add_input'
         # so users can see the first layer as input
         # Here we prefix the layer name by 'input_'
-        # OR, in case, if this layer takes input from a layer 
-        # that is now pefixed with 'input_', we need to adjust input layer names 
+        # OR, in case, if this layer takes input from a layer
+        # that is now pefixed with 'input_', we need to adjust input layer names
         if layer_nb in inputs:
             input_layer_names = [name]
             name = 'input_' + name
@@ -135,11 +261,12 @@ def create_model(layers, phase, input_dim, debug=False):
             input_layer_names = []
             for input_layer in input_layers:
                 if input_layer in inputs and in_deploy_mode:
-                    input_layer_names.append('input_' + layers[input_layer].name)
+                    #input_layer_names.append('input_' + layers[input_layer].name)
+                    input_layer_names.append(layers[input_layer].name)
                 else:
                     input_layer_names.append(layers[input_layer].name)
 
-        # outputs nodes are marked with 'output_' prefix 
+        # outputs nodes are marked with 'output_' prefix
         # from which output is derived later in 'add_output'
         if layer_nb in network_outputs:
             name = 'output_' + name
@@ -196,9 +323,9 @@ def create_model(layers, phase, input_dim, debug=False):
             if len(model.nodes[input_layer_name].output_shape[1:]) > 1:
                 model.add_node(Flatten(), name=name + '_flatten', input=input_layer_name)
                 input_layer_name = name + '_flatten'
-            
+
             model.add_node(Dense(output_dim), name=name, input=input_layer_name)
-           
+
         elif type_of_layer == 'lrn':
             alpha = layer.lrn_param.alpha
             k = layer.lrn_param.k
@@ -262,7 +389,7 @@ def create_model(layers, phase, input_dim, debug=False):
     for network_output in network_outputs:
         input_layer_name = 'output_' + layers[network_output].name
         model.add_output(name=layers[network_output].name, input=input_layer_name)
-    
+
     return model
 
 
@@ -271,7 +398,6 @@ def rot90(W):
         for j in range(W.shape[1]):
             W[i, j] = np.rot90(W[i, j], 2)
     return W
-
 
 def convert_weights(param_layers, v='V1', debug=False):
     weights = {}
@@ -304,7 +430,7 @@ def convert_weights(param_layers, v='V1', debug=False):
             weights_p = weights_p.T     # need to swapaxes here, hence transpose. See comment in conv
             weights_b = np.array(blobs[1].data)
             layer_weights = [weights_p.astype(dtype=np.float32), weights_b.astype(dtype=np.float32)]
-            
+
             weights[layer.name] = layer_weights
 
         elif typ == 'convolution':
@@ -365,9 +491,13 @@ def convert_weights(param_layers, v='V1', debug=False):
     return weights
 
 
-def load_weights(model, weights):
-    for layer in model.nodes:
-        if weights.has_key(layer):
-            model.nodes[layer].set_weights(weights[layer])
-
-
+def load_weights(model, weights, network_type):
+    if network_type == 'Sequential':
+        for layer_nb in range(0, len(model.layers)):
+            layer = model.layers[layer_nb]
+            if weights.has_key(layer.name):
+                model.layers[layer_nb].set_weights(weights[layer.name])
+    else:
+        for layer in model.nodes:
+            if weights.has_key(layer):
+                model.nodes[layer].set_weights(weights[layer])
